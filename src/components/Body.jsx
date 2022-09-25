af4e1a8c92e061040e3cf16e99998634aae278ef
Mdshahbazalam6 <shahbazalam2002620@gmail.com>
2022-09-25 07:20:22 +0530

    sidebar and main body almost completed

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import './body.css'
+import RightPanel from './RightPanel'
 import Sidebar from './Sidebar'
 const Body = () => {
   return (
     <div className='bodyContainer'>
      <Sidebar />
-     <div className='Main'></div>
-
+     {/* <div className='Main'></div> */}
+     < RightPanel />
     </div>
   )
 }
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,31 +1,49 @@
 import React from 'react'
-
+import './leftpanl.css'
 const Sidebar = () => {
   return (
-    <div className='leftPanel'>
-     <div className="leftpanelFirstChild ">
-     <div><span class="material-symbols-outlined icon">lightbulb</span></div>
-     <div className='sidebarName'>Notes</div>
-     </div>
-     <div className="leftPanelSecChild">
-      <div>     <span class="material-symbols-outlined icon">notifications</span></div>
-      <div className='sidebarName'>Reminders</div>
-     </div>
-     <div className="leftPanelThirdChild">
+  <div className="leftPanelContainer">
+      <div className='leftpanelFirstChild'>
+      <div className='leftPanel'>
+     <div id='lightIcon'><span class="material-symbols-outlined icon">lightbulb</span></div>
+     <div><span class="material-symbols-outlined icon">notifications</span></div>
      <div><span class="material-symbols-outlined icon">edit</span></div>
-     <div className='sidebarName'>Edit Labels</div>
-     </div>
-     <div className="leftPanelFourthChild">
-    <div> <span class="material-symbols-outlined icon">system_update_alt</span></div>
-    <div className='sidebarName'> Archive</div>
-     </div>
-     <div className="leftPanelFifthChild">
-    <div>
-    <span class="material-symbols-outlined icon">delete</span>
+     <div> <span class="material-symbols-outlined icon">system_update_alt</span></div>
+    <div><span class="material-symbols-outlined icon">delete</span></div>
     </div>
-    <div className='sidebarName'>Bin</div>
-     </div>
+      <div className='leftPanel2'>
+      <div className='sidebarName' id='notes'>Notes</div>
+      <div className='sidebarName'>Reminders</div>
+      <div className='sidebarName'>Edit Labels</div>
+      <div className='sidebarName'> Archive</div>
+      <div className='sidebarName'>Bin</div>
+      </div>
     </div>
+  </div>
+    // <div className='leftPanel'>
+    //  <div className="leftpanelFirstChild ">
+    //  <div><span class="material-symbols-outlined icon">lightbulb</span></div>
+    //  <div className='sidebarName'>Notes</div>
+    //  </div>
+    //  <div className="leftPanelSecChild">
+    //   <div>     <span class="material-symbols-outlined icon">notifications</span></div>
+    //   <div className='sidebarName'>Reminders</div>
+    //  </div>
+    //  <div className="leftPanelThirdChild">
+    //  <div><span class="material-symbols-outlined icon">edit</span></div>
+    //  <div className='sidebarName'>Edit Labels</div>
+    //  </div>
+    //  <div className="leftPanelFourthChild">
+    // <div> <span class="material-symbols-outlined icon">system_update_alt</span></div>
+    // <div className='sidebarName'> Archive</div>
+    //  </div>
+    //  <div className="leftPanelFifthChild">
+    // <div>
+    // <span class="material-symbols-outlined icon">delete</span>
+    // </div>
+    // <div className='sidebarName'>Bin</div>
+    //  </div>
+    // </div>
   )
 }
 
